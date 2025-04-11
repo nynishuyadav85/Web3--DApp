dc409bca9fa95559b06b9b20201310cbac1b6817
Nishant Yadav <nynishuyadav85@gmail.com>
2025-04-11 23:36:40 +0530

    added logic to update amount dynamically

--- src/AirDrop.jsx
+++ src/AirDrop.jsx
@@ -1,10 +1,21 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import React from 'react'
 
 const AirDrop = () => {
+
+    const wallet = useWallet();
+    const { connection } = useConnection()
+
+    async function sendAirDrop() {
+        const amount = document.getElementById("useramount").value
+        console.log(amount)
+        await connection.requestAirdrop(wallet.publicKey, amount * 100000000)
+    }
     return (
         <div>
-            <input type='number' placeholder='Amount'></input>
-            <button>Send AirDrop</button>
+            <input id='useramount' type='number' placeholder='Amount'></input>
+            <button onClick={sendAirDrop}>Send AirDrop</button>
+            <h3>Hi Nishu your public key is {wallet.publicKey?.toString()}</h3>
         </div>
     )
 }
