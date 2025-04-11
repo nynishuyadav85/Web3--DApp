6a0d963ce41ce4422ec2932126a423186c07e6fd
Nishant Yadav <nynishuyadav85@gmail.com>
2025-04-11 23:36:13 +0530

    updated endpoint url

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import './App.css'
-import { WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import AirDrop from './AirDrop'
 
 function App() {
 
   return (
-    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+    <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/YOGg5RV2dtrnvzQuYNSRwwYb8AiOnBUv"}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
           <WalletMultiButton />
